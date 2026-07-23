d3f14770ee78c76be3a79a118b1ad45d457f00e6
Jeff Magill <7297539+jefemagril@users.noreply.github.com>
2026-07-22 23:02:19 -0400

    Update assembly reference in init_52A0.c to match new function location for func_100052A0

--- conker/src/init_52A0.c
+++ conker/src/init_52A0.c
@@ -4,4 +4,4 @@
 #include "variables.h"
 
 /* PRE-NMI reset thread (PD resetproc). C match needs non-CSE access to D_8003BC20. */
-#pragma GLOBAL_ASM("asm/nonmatchings/init_50A0/func_100052A0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/init_52A0/func_100052A0.s")
